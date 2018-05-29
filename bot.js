66e32daf5ae61f130ec9ff5e80ef54cb89e4f24e
FELIPERIN <39714783+FELIPERIN@users.noreply.github.com>
2018-05-29 15:13:35 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,7 +51,7 @@ client.on('message', message => {
     // we ignore it
     if (!message.guild) return;
   
-    if (message.content.startsWith(prefix + "join")){
+    if (message.content === prefix + '/join'){
       // Only try to join the sender's voice channel if they are in one themselves
       if (message.member.voiceChannel) {
         message.member.voiceChannel.join()
