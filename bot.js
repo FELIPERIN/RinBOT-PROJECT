e8f3f901cd200ea20ebc98b7a5ea61d913d9c56a
FELIPERIN <39714783+FELIPERIN@users.noreply.github.com>
2018-05-28 20:02:31 -0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "Rin!";
+
+message1 = 'Olá!';
+message2 = 'Tchau!';
+message3 = 'Como você está?';
+
+client.on('ready', () => {
+    console.log('Pronto para o serviço.');
+  }); 
+
+client.on ('message' , (message)=>{//Evento
+        if(!message.content.startsWith(prefix) || message.author.bot) return ;//author.bot  = se o autor da mensagem for um bot 
+        //O resto do codigo
+        if(message.content.startsWith(prefix + "avatar")){
+            (message.reply(message.author.avatarURL))//Avatar do autor
+        }
+        if(message.content.startsWith(prefix + 'ping')){
+            (message.channel.send('Pong! Your ping is `' + `${Date.now() - message.createdTimestamp}` + ' ms`'))//comando para latencia do bot
+        }
+        if (message.content.startsWith(prefix + "criador")){
+            (message.reply('FELIPERIN#0001'))
+        }
+        
+})
+
+client.login(process.env.NDUwNzgxMDQ0NDU1NjM3MDIy.De4ljA.Rx-zxEHNY4O9IIJ2n7Bp_yCVxwE);
