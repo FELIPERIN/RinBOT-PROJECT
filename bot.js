8a7bf5a83385612e0a4b4fee522b20f9aee34ea7
Felipe <39714783+FELIPERIN@users.noreply.github.com>
2018-05-31 15:52:58 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,6 +26,9 @@ client.on ('message' , (message)=>{//Evento
         if(message.content.startsWith(prefix + "invite")){
             (message.channel.send('https://discordapp.com/api/oauth2/authorize?client_id=450781044455637022&permissions=201706560&scope=bot'))
         }
+        if(message.content.startsWith(prefix + "vitoria")){
+            (message.channel.send('ela é muito noob')
+        }
         if(message.content.startsWith(prefix + "help")){
             (message.channel.send({embed: { //faz um "embed"
                 color: 3447003,
