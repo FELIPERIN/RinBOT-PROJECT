773c3e8257a05c7ab72b20bb84e88bbad264dc4d
FELIPERIN <39714783+FELIPERIN@users.noreply.github.com>
2018-05-30 21:59:14 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const prefix = "r!"
 client.on('ready', () => {
     console.log('Pronto para o serviço.');
     client.user.setStatus('Online')
-    client.user.setGame('r!help')
+    bot.user.setPresence({ game: { name: 'r!help', type: 0 } })
   }); 
 
 client.on ('message' , (message)=>{//Evento
